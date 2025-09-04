287fcc8d3ff6956a8d531e8aa890c3d20cac398c
kebold123 <Dnepluev08@gmail.com>
2025-09-04 15:28:33 +0300

    add vaus and colors

--- vaus.py
+++ vaus.py
@@ -1,5 +1,11 @@
 import pygame
+from config import *
 
 class Vaus():
     def __init__(self):
-        self.position = 
+        self.position = (SCREEN_WIDGHT // 2 - 10, CENTER[1])
+        self.size = (20, 10)
+        self.speed = 5
+        self.color = (255, 255, 255) 
+
+    
