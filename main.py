a54c446a45f35bd6aa92dc2f73aa32e2b843c810
kebold123 <Dnepluev08@gmail.com>
2025-09-04 16:42:34 +0300

    fix vaus and add movement logic to it

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame
 import sys
 
+from vaus import Vaus
+from config import *
+
 def handle_keys():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -9,12 +12,27 @@ def handle_keys():
 
 screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Arcanoid")
-
+clock = pygame.time.Clock()
 
 pygame.init()
 
+vaus = Vaus()
+
 while True:
+    clock.tick(10)
+    screen.fill(BACKGROUND_COLOR)
+
     handle_keys()
 
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+        vaus.next_direction = "left"
+    elif keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+        vaus.next_direction = "right"
+
+    vaus.update_direction()
+
+    vaus.move()
+    vaus.draw(screen)
 
     pygame.display.flip()
--- vaus.py
+++ vaus.py
@@ -3,21 +3,30 @@ from config import *
 
 class Vaus():
     def __init__(self):
-        self.position = (SCREEN_WIDGHT // 2 - 10, CENTER[1])
+        self.position = (SCREEN_WIDGHT // 2 - VAUS_SIZE[0], SCREEN_HEIGHT // 8 * 7)
         self.last_position = None
-        self.size = (20, 10)
-        self.speed = 10
-        self.color = (255, 255, 255) 
+        self.size = VAUS_SIZE
+        self.speed = VAUS_SPEED
+        self.color = VAUS_COLOR 
         self.direction = None
+        self.next_direction = None
+
+    def update_direction(self):
+        self.direction = self.next_direction
+        self.next_direction = None
 
     def draw(self, screen):
         rect = pygame.Rect(self.position, self.size)
         pygame.draw.rect(screen, self.color, rect)
 
-    def move(self, direction=None):
-        if direction == None:
-            pass
-        elif direction == "left":
-            self.position = (self.position[0] - self.speed, self.position[1])
-        elif direction == "right":
-            self.position = (self.position[0] + self.speed, self.position[1])
+    def move(self):
+        if self.direction == "left":
+            if self.position[0] - self.speed <= 0:
+                self.position = (0, self.position[1])
+            else:
+                self.position = (self.position[0] - self.speed, self.position[1])
+        elif self.direction == "right":
+            if self.position[0] + self.speed + self.size[0] >= SCREEN_WIDGHT:
+                self.position = (SCREEN_WIDGHT - self.size[0], self.position[1])
+            else:
+                self.position = (self.position[0] + self.speed, self.position[1])
