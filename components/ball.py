6671349ae38bd54404d5fab7842c57858052cf3f
kebold123 <Dnepluev08@gmail.com>
2025-09-14 23:14:17 +0300

    the code has been tidied up

--- components/ball.py
+++ components/ball.py
@@ -17,12 +17,9 @@ class Ball():
         self.size = BALL_SIZE
         self.color = BALL_COLOR
         self.in_game = check_ingame(self.position)
-        self.speed = 5
-        self.v = self.speed
-        self.t = self.speed // 60
-        self.m = self.size * 2
-        self.cos = math.cos(math.radians(45))
-        self.sin = math.sin(math.radians(45))
+        self.m = self.size * 2 
+        self.x_v = 5
+        self.y_v = 5
     
     def draw(self, screen):
         if self.in_game:
@@ -30,7 +27,8 @@ class Ball():
 
     def move(self):
         if self.in_game:
-            self.position = (self.position[0] + self.v * self.cos, self.position[1] - self.v * self.sin)
-        else:
-            self.position = (self.position[0], self.position[1] + self.speed)
-            self.in_game = check_ingame(self.position)
+            self.position = (self.position[0] + self.x_v, self.position[1] - self.y_v)
+
+    def check_collision(self, vaus, bricks):
+        pass
+        
--- main.py
+++ main.py
@@ -8,7 +8,11 @@ from config import *
 from components.settings import *
 from random import randint
 
-
+def create_screen():
+    screen = pygame.display.set_mode((800, 600))
+    pygame.display.set_caption("Arcanoid")
+    clock = pygame.time.Clock()
+    return screen, clock
 
 def handle_esc():
     for event in pygame.event.get():
@@ -31,56 +35,73 @@ def handle_esc():
 
 
 def draw_level(positions, screen):
+    bricks = []
     for i in positions:
         brick = Brick(position=i)
+        bricks.append(brick)
         brick.draw(screen)
+    return bricks
 
+def game_menu(screen):
+    screen.fill(BACKGROUND_COLOR)
+    play_bt.draw(screen)
+    level_bt.draw(screen)
 
-screen = pygame.display.set_mode((800, 600))
-pygame.display.set_caption("Arcanoid")
-clock = pygame.time.Clock()
+def create_variables():
+    vaus = Vaus()
+    game_set = Game_settings()
+    play_bt = Play()
+    level_bt = Level(play_bt.size[1] + 10)
+    ball = Ball()
+    return vaus, game_set, play_bt, level_bt, ball
 
+def ball_action(ball, vaus, bricks, screen):
+    if ball.in_game:
+        ball.move()
+        ball.check_collision(vaus, bricks)
+        ball.draw(screen)
+
+def vaus_move(keys, vaus):
+    if keys[pygame.K_LEFT]:
+        vaus.next_direction = "left"
+    elif keys[pygame.K_RIGHT]:
+        vaus.next_direction = "right"
+
+def vaus_actions(screen, vaus):
+    vaus.update_direction()
+    vaus.move()
+    vaus.draw(screen)
+
+def change_level(level_bt):
+    if level_bt.change_level:
+        level_bt.positions_for_bricks = positions_breaks(level_bt.level)
+        level_bt.change_level = False
+
+pygame.font.init()
 pygame.init()
 
-vaus = Vaus()
-game_set = Game_settings()
-play_bt = Play()
-level_bt = Level(play_bt.size[1] + 10)
-ball = Ball()
+vaus, game_set, play_bt, level_bt, ball = create_variables()
+screen, clock = create_screen()
+
 
 
 while True:
     handle_esc()
 
     if game_set.game_menu:
-        screen.fill(BACKGROUND_COLOR)
-        play_bt.draw(screen)
-        level_bt.draw(screen)
 
-        if level_bt.change_level:
-            level_bt.positions_for_bricks = positions_breaks(level_bt.level)
-            print(level_bt.positions_for_bricks)
-            level_bt.change_level = False
+        game_menu(screen)
+        change_level(level_bt)
 
         clock.tick(15)
     else:
         clock.tick(35)
         screen.fill(BACKGROUND_COLOR)
 
-        draw_level(level_bt.positions_for_bricks, screen)
+        bricks = draw_level(level_bt.positions_for_bricks, screen)
+        ball_action(ball, vaus, bricks, screen)
 
-        ball.move()
-        ball.draw(screen)
-
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT] or keys[pygame.K_a]:
-            vaus.next_direction = "left"
-        elif keys[pygame.K_RIGHT] or keys[pygame.K_d]:
-            vaus.next_direction = "right"
-
-        vaus.update_direction()
-
-        vaus.move()
-        vaus.draw(screen)
+        vaus_move(pygame.key.get_pressed(), vaus)
+        vaus_actions(screen, vaus)
 
     pygame.display.flip()
